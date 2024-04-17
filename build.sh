3bf6bafb25f08cf051ab184fc68a66c8457b2784
rexim <reximkut@gmail.com>
2024-04-18 04:28:09 +0700

    Build all the executables

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -xe
 
 mkdir -p build/
 
-# rustc -o build/tula src/tula.rs
-# rustc -o build/test_lexer src/test_lexer.rs
+rustc -o build/tula src/tula.rs
+rustc -o build/test_lexer src/test_lexer.rs
 rustc -o build/test_sexpr src/test_sexpr.rs
